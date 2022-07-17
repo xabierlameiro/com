15211b5b376b9848cb2faa0b9a2d68e81299010f
Xabier Lameiro Cardama <xabier.lameirocardama@plexus.es>
2022-07-17 18:03:30 +0200

    Coverage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,12 @@
 import type { ReactElement } from 'react'
-import type { NextPageWithLayout } from './_app'
 import Layout from '@/layout'
 
-const HomePage: NextPageWithLayout = () => {
+const HomePage = (): ReactElement => {
 	return <h1>Hello JSX</h1>
 }
 
-HomePage.getLayout = function getLayout(page: ReactElement) {
-	return <Layout>{page}</Layout>
+HomePage.getLayout = function getLayout(page: ReactElement, meta: any) {
+	return <Layout meta={meta}>{page}</Layout>
 }
 
 export default HomePage
